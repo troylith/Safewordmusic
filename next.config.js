9500f4c93ee1aaac0c9a33ecf5b4e310ddeada33
troylith <troynatureguide@gmail.com>
2025-08-07 06:35:55 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-module.exports = { reactStrictMode: true };
-// next.config.js
-module.exports = {
-  env: {
-    NEXT_PUBLIC_AI_KEY: process.env.NEXT_PUBLIC_AI_KEY,
-    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  },
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
